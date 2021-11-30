2893330bdee5d540549688dc0a79a0b1dc3c660e
m0ksem <m0ksem1337@gmail.com>
2021-11-30 21:10:57 +0200

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="app">
+    <va-card>
+      <va-card-content>
+        <h1 class="display-2">{{ currentTime }}</h1>
+        <va-divider />
+      </va-card-content>
+    </va-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const currentTime = ref(new Date().toLocaleTimeString())
+
+    setInterval(() => {
+      currentTime.value = new Date().toLocaleTimeString()
+    }, 1000)
+
+    return {
+      currentTime,
+    }
+  },
+})
+</script>
+
+
+<!-- <script lang="ts">
+export default {
+  data() {
+    return {
+      currentTime: '',
+    }
+  },
+  
+  mounted() {
+    setInterval(() => {
+      this.currentTime = new Date().toTimeString()
+    }, 1000)
+  }
+}
+</script> -->
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100wh;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { VuesticPlugin } from 'vuestic-ui' // <-
+import 'vuestic-ui/dist/vuestic-ui.css' // <-
+
+createApp(App).use(VuesticPlugin).mount('#app')

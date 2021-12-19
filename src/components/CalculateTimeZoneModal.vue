17304ace558d117e4e659831fda1b67c5b3e16aa
Darya Volkova <anrich@bk.ru>
2021-12-19 03:53:21 +0200

    Added modal window and button

---
+++ src/components/CalculateTimeZoneModal.vue
@@ -0,0 +1,24 @@
+<template>
+    <va-modal :model-value="modelValue" @update:model-value="$emit('update:model-value', $event)">
+
+    </va-modal>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+props: {
+    modelValue: { type: Boolean, required: true },
+  },
+
+    setup(props) {
+    
+    },
+})
+</script>
+
+
+<style scoped>
+
+</style>
--- src/components/TimeZoneCard.vue
+++ src/components/TimeZoneCard.vue
@@ -12,95 +12,122 @@
           <span>
             {{ name }}
           </span>
-          
+
           <p>
-            <span class="title">Offset {{ timeZoneOffset / 60 }}h</span> 
+            <span class="title">Offset {{ timeZoneOffset / 60 }}h</span>
           </p>
         </div>
 
-        <va-button 
-          class="time-zone-card__delete-button" 
-          v-show="hover" icon="close" size="small" 
+        <va-button
+          class="time-zone-card__delete-button"
+          v-show="hover"
+          icon="cancel"
+          flat
           color="danger"
           @click="$emit('delete')"
         />
+
+        <va-button
+          class="time-zone-card__calculate-button"
+          flat
+          v-show="hover"
+          icon="schedule"
+          @click="toShowCalculate=true"
+        />
+
+        <CalculateTimeZoneModal v-model="toShowCalculate"></CalculateTimeZoneModal>
       </va-card-content>
     </va-hover>
   </va-card>
-
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue'
-import { useInterval } from '../hooks/useInterval'
-import { stringTimeZoneToNumber } from '../utils/stringTimeZoneToNumber'
+import { computed, defineComponent, ref } from "vue";
+import { useInterval } from "../hooks/useInterval";
+import { stringTimeZoneToNumber } from "../utils/stringTimeZoneToNumber";
+import CalculateTimeZoneModal from "./CalculateTimeZoneModal.vue";
 
 export default defineComponent({
+  components: {
+    CalculateTimeZoneModal,
+  },
+
   props: {
     name: {
-      type: String, default: "Kyiv",
+      type: String,
+      default: "Kyiv",
     },
     timezone: {
-      type: Object, required: true
+      type: Object,
+      required: true,
     },
     color: {
-      type: String
+      type: String,
     },
     ampm: {
       type: Boolean,
       default: false,
-    }
+    },
   },
-  
-  setup(props) {
-    const userOffset = new Date().getTimezoneOffset()
 
-    const timeZoneOffset = computed(() =>
-      stringTimeZoneToNumber(props.timezone.utc) + userOffset
-    )
+  setup(props) {
+    const toShowCalculate = ref(false);
+
+    const userOffset = new Date().getTimezoneOffset();
+
+    const timeZoneOffset = computed(
+      () => stringTimeZoneToNumber(props.timezone.utc) + userOffset
+    );
 
     const getDateWithOffset = () => {
-      const now = new Date()
+      const now = new Date();
 
-      now.setTime(now.getTime() + timeZoneOffset.value * 1000 * 60)
+      now.setTime(now.getTime() + timeZoneOffset.value * 1000 * 60);
 
-      return now
-    }
+      return now;
+    };
 
-    const  formatDate = (date:Date) => {
+    const formatDate = (date: Date) => {
       if (props.ampm == true) {
-        return date.toLocaleTimeString('en-US')
+        return date.toLocaleTimeString("en-US");
       } else {
-        return date.toLocaleTimeString('en-GB')
+        return date.toLocaleTimeString("en-GB");
       }
-    }
+    };
 
-    const currentTime = ref(formatDate(getDateWithOffset()))
+    const currentTime = ref(formatDate(getDateWithOffset()));
 
     useInterval(() => {
-      currentTime.value = formatDate(getDateWithOffset())
-    }, 1000)
+      currentTime.value = formatDate(getDateWithOffset());
+    }, 1000);
 
     return {
       currentTime,
 
-      timeZoneOffset
-    }
+      timeZoneOffset,
+
+      toShowCalculate,
+    };
   },
-})
+});
 </script>
 
 <style lang="scss" scoped>
-  .title {
-    text-align: left;
-  }
+.title {
+  text-align: left;
+}
 
-  .time-zone-card {
-    position: relative;
-    &__delete-button {
-      position: absolute;
-      right: 16px;
-      top: 16px;
-    }
+.time-zone-card {
+  position: relative;
+  &__delete-button {
+    position: absolute;
+    right: 16px;
+    top: 8px;
   }
+  &__calculate-button {
+    position: absolute;
+    right: 54px;
+    top: 8px;
+  }
+}
 </style>

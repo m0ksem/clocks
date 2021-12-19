e75c36d02baf372837bb2b5a87e233a9cddc7a6c
Darya Volkova <anrich@bk.ru>
2021-12-19 04:39:43 +0200

    CalculateTimeZone is working now

--- src/components/CalculateTimeZoneModal.vue
+++ src/components/CalculateTimeZoneModal.vue
@@ -3,13 +3,13 @@
     :model-value="modelValue"
     @update:model-value="$emit('update:model-value', $event)"
   >
-    <va-time-input label="Your time" v-model="urTime"> </va-time-input>
+    <va-time-input label="Your time" v-model="urTime" class="mb-2"> </va-time-input>
     <va-time-input label="Card time" v-model="cardTime"> </va-time-input>
   </va-modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, watch } from "vue";
 
 export default defineComponent({
   props: {
@@ -19,16 +19,24 @@ export default defineComponent({
 
   setup(props) {
     const urTime = ref(new Date());
-
     const getDateWithOffset = () => {
       const now = new Date();
-
       now.setTime(now.getTime() + props.offset * 1000 * 60);
 
       return now;
     };
-
     const cardTime = ref(getDateWithOffset());
+    watch(urTime, () => {
+      const now = new Date();
+      now.setTime(urTime.value.getTime() + props.offset * 1000 * 60);
+      cardTime.value = now
+    });
+    watch(cardTime, () => {
+      const now = new Date();
+      now.setTime(cardTime.value.getTime() - props.offset * 1000 * 60);
+      urTime.value = now
+    });
+
     return {
       urTime,
       cardTime,

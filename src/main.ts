216982695d17ef3b81d8de42718bc338b22b7be2
m0ksem <m0ksem1337@gmail.com>
2022-07-21 21:25:15 +0300

    Bump vuestic-ui to 1.4.7

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createVuestic } from 'vuestic-ui'
 import { createPinia } from 'pinia'
-import 'vuestic-ui/css'
+import 'vuestic-ui/styles/essential.css'
 import './scss/vuestic-overrides.scss'
 
 createApp(App)
